9f11d684f497d56f4c0450c7b96f9a27c58f26f7
prajeshy <71869555+prajeshy@users.noreply.github.com>
2020-10-08 03:52:32 -0700

    Add files via upload
    This code is written in c only for getting the tables.
    {JUST FOR HACTOBERFEST}

---
+++ table.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+
+int main() 
+{
+    int a;
+    printf("Enter the number : \n");
+    scanf("%d" , &a);
+    printf("6*1 = %d\n", a*1 );
+    printf("6*2 = %d\n", a*2 );
+    printf("6*3 = %d\n", a*3 );
+    printf("6*4 = %d\n", a*4 );
+    printf("6*5 = %d\n", a*5 );
+    printf("6*6 = %d\n", a*6 );
+    printf("6*7 = %d\n", a*7 );
+    printf("6*8 = %d\n", a*8 );
+    printf("6*9 = %d\n", a*9 );
+    printf("6*10 = %d\n", a*10 );
+    return 0;
+}

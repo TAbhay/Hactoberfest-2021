3299a23e59b12ea5d7f1a8e0e0d6b4846ed97026
Deepika1108 <57054910+Deepika1108@users.noreply.github.com>
2020-10-09 07:58:48 +0530

    updated checkprime.py
    Another way to check prime number

--- Python_programming/CheckPrime.py
+++ Python_programming/CheckPrime.py
@@ -14,3 +14,17 @@ if count != 2:
         print('Number is not prime')        
 else:
             print("Number is prime")
+        
+        
+        
+#Another way to check prime number
+num=13
+if num > 1:
+    for i in range(1,num//1):
+        if (num % i) == 0:
+            print(num,"is not a prime number")
+            break
+            else:
+                print(num, "is a prime number")
+    
+

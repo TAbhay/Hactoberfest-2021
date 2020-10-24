8dcfa026a0e5d6448e6256b290aaa08bb0b8d150
Shayan Halder <shayanhalder2389@gmail.com>
2020-10-24 11:43:18 -0700

    Update array_diff.py

--- Python_programming/array_diff.py
+++ Python_programming/array_diff.py
@@ -1,4 +1,4 @@
-# Function taht takes in two arrays and returns the difference between them, which is the value that is not common to both arrays. 
+# Function that takes in two arrays and returns the difference between them, which is the value that is not common to both arrays. 
 # Example output: arrayDiff([9, 3, 2, 1], [2, 3]) ==> [9, 1]
 
 def array_diff(a, b):

a8d5a754d18043bc04c1380394d8bd2f478e17f7
DevanshPurwarDWD <75568315+DevanshPurwarDWD@users.noreply.github.com>
2021-10-01 14:13:42 +0530

    selection_sort

---
+++ CP_Resources/Sorting/selection_sort.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+
+public class selection_sort {
+
+    public static void main(String[] args) {
+
+
+        int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
+
+        for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0;
+                lastUnsortedIndex--) {
+
+            int largest = 0;
+
+            for (int i = 1; i <= lastUnsortedIndex; i++) {
+                if (intArray[i] > intArray[largest]) {
+                    largest = i;
+                }
+            }
+
+            swap(intArray, largest, lastUnsortedIndex);
+
+        }
+
+        System.out.println(Arrays.toString(intArray));
+    }
+
+    public static void swap(int[] array, int i, int j) {
+
+        if (i == j) {
+            return;
+        }
+
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+
+    }
+
+}

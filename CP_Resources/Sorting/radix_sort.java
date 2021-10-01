6255ffa174e18b73df78260788cc4bf6647e80f8
DevanshPurwarDWD <75568315+DevanshPurwarDWD@users.noreply.github.com>
2021-10-01 14:12:42 +0530

    radix sort

---
+++ CP_Resources/Sorting/radix_sort.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+
+public class radix_sort {
+
+    public static void main(String[] args) {
+
+        int[] radixArray = { 4725, 4586, 1330, 8792, 1594, 5729 };
+
+        radixSort(radixArray, 10, 4);
+
+        System.out.println(Arrays.toString(radixArray));
+    }
+
+    public static void radixSort(int[] input, int radix, int width) {
+        for (int i = 0; i < width; i++) {
+            radixSingleSort(input, i, radix);
+        }
+    }
+
+    public static void radixSingleSort(int[] input, int position, int radix) {
+
+        int numItems = input.length;
+        int[] countArray = new int[radix];
+
+        for (int value : input) {
+            countArray[getDigit(position, value, radix)]++;
+        }
+        // Adjust the count array
+        for (int j = 1; j < radix; j++) {
+            countArray[j] += countArray[j - 1];
+        }
+
+        int[] temp = new int[numItems];
+        for (int tempIndex = numItems - 1; tempIndex >= 0; tempIndex--) {
+            temp[--countArray[getDigit(position, input[tempIndex], radix)]] = input[tempIndex];
+        }
+
+        for (int tempIndex = 0; tempIndex < numItems; tempIndex++) {
+            input[tempIndex] = temp[tempIndex];
+        }
+
+    }
+
+    public static int getDigit(int position, int value, int radix) {
+        return value / (int) Math.pow(radix, position) % radix;
+    }
+
+}

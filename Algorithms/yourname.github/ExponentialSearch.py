4ade083157d35f6fabd16fe8b7a1e9c6142215ea
pseudonerd16 <sreejitaroytiu1923@gmail.com>
2021-10-01 14:36:48 +0530

    added exponential search, fibonacci search and interpolation search

---
+++ Algorithms/yourname.github/ExponentialSearch.py
@@ -0,0 +1,7 @@
+def ExponentialSearch(lys, val):
+    if lys[0] == val:
+        return 0
+    index = 1
+    while index < len(lys) and lys[index] <= val:
+        index = index * 2
+    return BinarySearch( arr[:min(index, len(lys))], val)

165815e37d84a4d78d69747a4df00846dd0951cf
DevanshPurwarDWD <75568315+DevanshPurwarDWD@users.noreply.github.com>
2021-10-01 14:07:08 +0530

    counting sort

---
+++ CP_Resources/Sorting/counting_sort.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class counting_sort {
+
+    public static void main(String[] args) {
+        int[] intArray = { 2, 5, 9, 8, 2, 8, 7, 10, 4, 3 };
+
+        countingSort(intArray, 1, 10);
+
+        System.out.println(Arrays.toString(intArray));
+    }
+
+    public static void countingSort(int[] input, int min, int max) {
+
+        int[] countArray = new int[(max - min) + 1];
+
+        for (int i = 0; i < input.length; i++) {
+            countArray[input[i] - min]++;
+        }
+
+        int j = 0;
+        for (int i = min; i <= max; i++) {
+            while (countArray[i - min] > 0) {
+                input[j++] = i;
+                countArray[i - min]--;
+            }
+        }
+
+    }
+}

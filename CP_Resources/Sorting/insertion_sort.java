fd59c6270808a2d105c7f9e418e57287ee3db327
DevanshPurwarDWD <75568315+DevanshPurwarDWD@users.noreply.github.com>
2021-10-01 14:08:05 +0530

    insertion sort

---
+++ CP_Resources/Sorting/insertion_sort.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+public class insertion_sort {
+
+    public static void main(String[] args) {
+
+        int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
+
+        for (int firstUnsortedIndex = 1; firstUnsortedIndex < intArray.length;
+                firstUnsortedIndex++) {
+            int newElement = intArray[firstUnsortedIndex];
+
+            int i;
+
+            for (i = firstUnsortedIndex; i > 0 && intArray[i - 1] > newElement; i--) {
+                intArray[i] = intArray[i - 1];
+            }
+
+            intArray[i] = newElement;
+        }
+
+        System.out.println(Arrays.toString(intArray));
+
+    }
+
+}

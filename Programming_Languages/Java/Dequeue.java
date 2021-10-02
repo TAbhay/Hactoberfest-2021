30342732a3c2f55745eb9001df49898a2cb5bcc2
Srishti Gauraha <67550382+Srishti44-g@users.noreply.github.com>
2021-10-02 09:11:54 +0530

    Added Dequeue.java and Ladder.java file

---
+++ Programming_Languages/Java/Dequeue.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+ 
+public class Dequeue{
+    public static void main(String[] args)
+    {
+        Deque<Integer> DQ
+            = new LinkedList<Integer>();
+        DQ.add(7855642);
+        DQ.add(35658786);
+        DQ.add(5278367);
+        DQ.add(74381793);
+        System.out.println("Deque: " + DQ);
+    }
+}
---
+++ Programming_Languages/Java/Ladder.java
@@ -0,0 +1,17 @@
+//Demonstrate an if-else-if ladder.
+class Ladder{
+    public static void main(String args[]){
+     int x ;
+   for(x =0 ; x<6;x++)
+        if(x==1)
+           System.out.println("x is one");
+       else if(x==2)
+           System.out.println("x is two");
+        else if(x==3)
+           System.out.println("x is three");
+        else if(x==4)
+           System.out.println("x is  four");
+        else 
+            System.out.println("x is not between 1 and 4");
+ }
+}

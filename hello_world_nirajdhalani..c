1a1e3c5039b01675e74ed491fc5f77923922df29
Niraj Dhalani <32194632+nirajdhalani@users.noreply.github.com>
2021-10-04 12:37:07 +0530

    Create hello_world_nirajdhalani..c
    This program is about how to write hello world in C

---
+++ hello_world_nirajdhalani..c
@@ -0,0 +1,14 @@
+
+// LANGUAGE: C
+// AUTHOR: Dennis Ritchie
+// GITHUB: https://github.com/nirajdhalani
+
+
+
+
+include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   return 0;
+}

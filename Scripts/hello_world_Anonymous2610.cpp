48e465b78bf95961fc182623668a546988e81d9f
Anonymous2610 <78662053+Anonymous2610@users.noreply.github.com>
2021-10-05 18:18:47 +0530

    Add files via upload
    first commit in hactoberfest 2021

---
+++ Scripts/hello_world_Anonymous2610.cpp
@@ -0,0 +1,10 @@
+//LANGUAGE: C++
+// VS CODE
+//AUTHOR: CHANDAN KUMAR HABODA
+//GITHUB: https://github.com/Anonymous2610
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"hello world";
+    return 0;
+}

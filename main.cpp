4608f860a27b4f3e5b4eb7e6b9f657a07bac3916
Muhammad Ali Raza <47527115+Aliraza777@users.noreply.github.com>
2021-10-10 02:01:22 +0500

    added febonacci series function

--- main.cpp
+++ main.cpp
@@ -1,60 +1,23 @@
-#include<stdio.h>
+#include <iostream>
 
-
-void swapValue(int* a, int* b)
-{
-    int t = *a;
-    *a = *b;
-    *b = t;
-}
-
-int mPartition (int arr[], int low, int high)
-{
-    int pivot = arr[high];    // pivot
-    int i = (low - 1);  // Index of smaller element
-
-    for (int j = low; j <= high- 1; j++)
-    {
-
-        if (arr[j] <= pivot)
-        {
-            i++;
-            swapValue(&arr[i], &arr[j]);
-        }
-    }
-    swapValue(&arr[i + 1], &arr[high]);
-    return (i + 1);
-}
-
-
-void quickSort(int arr[], int low, int high)
-{
-    if (low < high)
-    {
-
-        int prt = mPartition(arr, low, high);
-
-
-        quickSort(arr, low, prt - 1);
-        quickSort(arr, prt + 1, high);
-    }
-}
-
-void printArray(int arr[], int n)
-{
-    int i;
-    for (i=0; i < n; i++)
-        printf("%d ", arr[i]);
-
-}
-
+using namespace std;
 
 int main()
 {
-    int arr[] = {10, 7, 8, 9, 1, 5};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    quickSort(arr, 0, n-1);
-    printf("Sorted array: n =");
-    printArray(arr, n);
+int x;
+cout<<"Enter any number!"<<endl;
+cin>>x;
+int y=0;
+int z=1;
+int v = 0;
+while(v<x)
+{
+    cout<<y+z<<endl;
+    int g = y;
+    y=z+y;
+    z=y;
+    v++;
+}
+
     return 0;
 }

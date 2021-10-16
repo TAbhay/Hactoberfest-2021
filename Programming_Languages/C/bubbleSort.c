552a870f5ec7b042b3d6516f4a812aeda54db356
Ashen Kavinda <49136460+Ashenk97@users.noreply.github.com>
2021-10-16 11:21:15 +0530

    Bubble sort in C

---
+++ Programming_Languages/C/bubbleSort.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+void bubble_sort(int a[], int n) {
+    int i = 0, j = 0, tmp;
+    for (i = 0; i < n; i++) {   
+        for (j = 0; j < n - i - 1; j++) { 
+            if (a[j] > a[j + 1]) {  
+                tmp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = tmp;
+            }
+        }
+    }
+}
+int main() {
+  int a[100], n, i, d, swap;
+  printf("Enter number of elements in the array:\n");
+  scanf("%d", &n); 
+  printf("Enter %d integers\n", n);
+  for (i = 0; i < n; i++)
+    scanf("%d", &a[i]);
+  bubble_sort(a, n);
+  printf("Printing the sorted array:\n");
+  for (i = 0; i < n; i++)
+     printf("%d\n", a[i]);
+  return 0;
+}

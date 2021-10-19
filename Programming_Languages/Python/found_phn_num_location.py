ef3ed34291b320c308c1420396d882802d6317e7
Bhumi lad <70130258+bhumilad@users.noreply.github.com>
2021-10-19 07:33:55 +0530

    Add files via upload

---
+++ Programming_Languages/Python/found_phn_num_location.py
@@ -0,0 +1,11 @@
+import pyttsx3
+friend = pyttsx3.init()
+friend.say("i am here for you")
+friend.runAndWait()
+friend.say("jerry what are you doing write now")
+friend.runAndWait()
+
+
+# a = [4,5,4,6,4,6,2,3,3,2,2,2,2,2,2,2]
+# min = min(set(a),key=a.count)
+# print(min)
